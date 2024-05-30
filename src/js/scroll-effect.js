0c0c587ed47cc966857cf484e7eedb4164142ad1
tbaek <roni2006@hanmail.net>
2024-05-30 12:25:39 -0400

    Main web design modification with typewriter feature.

--- src/js/scroll-effect.js
+++ src/js/scroll-effect.js
@@ -4,16 +4,20 @@ function getScrollPositionRatio() {
 
     // get current scroll position
     let scrollPosition = window.scrollY;
-    return scrollPosition / documentHeight
+    return (scrollPosition / documentHeight) * 2;
 }
 
-function componentTransparentOnScroll(componentId) {
+function componentTransparentOnScroll(componentId, isDarkMode) {
     let scrollWeight = Math.exp(-getScrollPositionRatio());
 
     const targetComponent = document.getElementById(componentId);
     //get svg component
     if (targetComponent) {
-        targetComponent.setAttribute('style', 'opacity:' + scrollWeight);
+        if (isDarkMode) {
+            targetComponent.setAttribute('style', 'opacity:' + (0.9 + Math.log2(0.7 +scrollWeight)));
+        } else {
+            targetComponent.setAttribute('style', 'opacity:' + scrollWeight);
+        }
     } else{
         console.log("Element with tag name '" + componentId + "' not found.");
     }
@@ -21,18 +25,62 @@ function componentTransparentOnScroll(componentId) {
     return scrollWeight;
 }
 
-function fixNavBarAtTop() {
+function fixNavBarAtTop(isDarkMode) {
     let scrollPosition = getScrollPositionRatio();
     let navBarComponent = document.getElementById("category_list");
+    let backgroundColor;
+    if (isDarkMode) {
+        backgroundColor = "linear-gradient(80deg, #975a5e 36%, rgba(0, 0, 0, 0) 30%), linear-gradient(110deg, #453953 60%, #25161b 58%)";
+    } else {
+        backgroundColor = "linear-gradient(80deg, #ea9085 36%, rgba(0, 0, 0, 0) 30%), linear-gradient(110deg, #c06c84 58%, #6c5b7c 58%)";
+    }
 
     if (navBarComponent) {
-        if (scrollPosition >= 0.125) {
-            navBarComponent.style.top = "1em";
+        if (scrollPosition >= 0.195) {
+            navBarComponent.style.top = "0";
             navBarComponent.style.position = "fixed";
             navBarComponent.style.zIndex = "1000";
+            navBarComponent.style.setProperty('background', backgroundColor);
         } else {
             navBarComponent.style.position = "inherit";
             navBarComponent.style.zIndex = "auto";
+            navBarComponent.style.background = "inherit"
         }
     }
+}
+
+function getScrollIntersectionObserver() {
+    return new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('active');
+            } else {
+                entry.target.classList.remove('active');
+            }
+
+            // Store flag in sessionStorage
+            sessionStorage.setItem('hasBeenCalled', 'true');
+        });
+    });
+}
+
+function setActiveObserverOnceByName(id, resetTime) {
+    let typeWriterActivatedOnce = sessionStorage.getItem(id + 'ActivatedOnce');
+    const observer = getScrollIntersectionObserver();
+
+    if(typeWriterActivatedOnce === "true") {
+        document.querySelectorAll('#' + id).forEach(e => {
+            observer.observe(e);
+        });
+
+        sessionStorage.setItem(id + 'ActivatedOnce', 'false');
+
+    }
+
+    setTimeout(() => {
+        document.querySelectorAll('#' + id).forEach(e => {
+            e.style.opacity = "1";
+            observer.unobserve(e);
+        });
+    }, resetTime);
 }
