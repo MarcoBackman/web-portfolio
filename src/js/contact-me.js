828e490357f1409441c5b12390f58b7bc45fc189
MarcoBackman <roni2006@hanmail.net>
2024-08-05 14:41:38 +0900

    Added message server url for message send

---
+++ src/js/contact-me.js
@@ -0,0 +1,38 @@
+const MESSAGE_SERVER = "http://3.39.236.44:9091/message-service/sendFeedbackMessage";
+const nameField = document.querySelector('#name-input');
+const emailField = document.querySelector('#address-input');
+const messageField = document.querySelector('#content-input');
+const sendButton = document.querySelector('#send-button');
+
+sendButton.addEventListener('click', function(e) {
+    // prevent the actual submit event
+    e.preventDefault();
+
+    // form data
+    const data = {
+        name: nameField.value,
+        email: emailField.value,
+        content: messageField.value
+    };
+
+    // make the request
+    fetch(MESSAGE_SERVER, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(function(response) {
+        if(response.ok) {
+            console.log('Message sent!');
+            // clear fields after successful send
+            nameField.value = '';
+            emailField.value = '';
+            messageField.value = '';
+        } else {
+            console.log('An error occurred while sending the message.');
+        }
+    }).catch(function(error) {
+        console.log('Fetch error: ', error);
+    });
+});
