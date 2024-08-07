795f8b2a445a8ed45eb337cb8286296c368f23b4
MarcoBackman <roni2006@hanmail.net>
2024-08-08 01:29:44 +0900

    Temporal website allow button added.

--- src/js/contact-me.js
+++ src/js/contact-me.js
@@ -2,7 +2,14 @@ const MESSAGE_SERVER = "https://3.39.236.44:9091/message-service/sendFeedbackMes
 const nameField = document.querySelector('#name-input');
 const emailField = document.querySelector('#address-input');
 const messageField = document.querySelector('#content-input');
+const enableSSL = document.querySelector('#allow-self-signed-host');
 const sendButton = document.querySelector('#send-button');
+
+enableSSL.addEventListener('click', function(e) {
+    e.preventDefault();
+    alert("인증서(CA) 등록 중. 계속 하려면 사이트 접속을 허가해주세요.");
+    window.open('https://3.39.236.44:9091/message-service/sendFeedbackMessage', '_blank');
+});
 
 sendButton.addEventListener('click', function(e) {
     // prevent the actual submit event
@@ -34,6 +41,7 @@ sendButton.addEventListener('click', function(e) {
             console.log('An error occurred while sending the message.');
         }
     }).catch(function(error) {
+        alert("사이트 허용을 해주세요.");
         console.log('Fetch error: ', error);
     });
 });
